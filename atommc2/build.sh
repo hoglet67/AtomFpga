3d5408533d973f8ebba0776757401f407010a93b
David Banks <dbanks@hp.com>
2014-05-29 22:19:32 +0100

    Added -DAVR to build.sh to compile AVR version
    Change-Id: I665387650c9667dca6c4971e98f681b9bbfc2898

--- atommc2/build.sh
+++ atommc2/build.sh
@@ -3,8 +3,8 @@
 rm -f *.rom
 
 echo Assembling
-ca65 -l atomm2.a000.lst -o a000.o atommc2.asm
-ca65 -l atomm2.e000.lst -o e000.o -D EOOO atommc2.asm
+ca65 -l atomm2.a000.lst -o a000.o -DAVR atommc2.asm
+ca65 -l atomm2.e000.lst -o e000.o -DAVR -D EOOO atommc2.asm
 
 echo Linking
 ld65 a000.o -o atommc2-2.9-a000.rom -C atommc2-a000.lkr 
