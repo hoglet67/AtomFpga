ec4d8fa1cb587417c5a25f95a7e8bc7ec6856fa0
David Banks <dbanks@hp.com>
2014-06-21 17:57:19 +0100

    Updated the ROMs and included AtomFpga Utils instead of SALFAA
    Change-Id: I9f36344afad1b7ea0289c28d8f42a6b6e854df52

---
+++ roms/README.txt
@@ -0,0 +1,32 @@
+00 - SDDOS v3.25
+01 - PCharme v1.73
+02 - Gags v2.3
+03 - AXR1
+04 - AtomFpga Utils v0.12 (was SALFAA v2.6)
+05 - Atomic Windows v1.1
+06 - WE ROM
+07 - Program Power Programmers Toolkit
+08 - Unused
+09 - Unused
+0A - Unused
+0B - Unused
+0C - Unused
+0D - Unused
+0E - Unused
+0F - Unused
+10 - Atom Basic (#C000)
+11 - Atom Floating Point (#D000)
+12 - Atom AtoMMC (#E000)
+13 - Atom Kernel (#F000)
+14 - BBC Basic 2nd 4K (#C000)
+15 - BBC Basic 3rd 4K (#D000)
+16 - BBC Basic 4th 4K (#E000)
+17 - BBC MOS (#F000)
+18 - BBC Basic 1st 4K (#A000)
+19 - Unused
+1A - Unused
+1B - Unused
+1C - Unused
+1D - Unused
+1E - Unused
+1F - Unused
---
+++ roms/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -f 128K.rom
+touch 128K.rom
+
+cat sddos-v3.25-avr.rom >> 128K.rom
+cat pcharme_v1.73.rom >> 128K.rom
+cat gags_v2.3.rom >> 128K.rom
+cat axr1.rom >> 128K.rom
+cat fpgautils_v0.12.rom >> 128K.rom
+cat atomic_windows_v1.1.rom >> 128K.rom
+cat we_rom.rom >> 128K.rom
+cat pp_toolkit.rom >> 128K.rom
+
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+
+cat abasic.rom >> 128K.rom
+cat afloat_patched.rom >> 128K.rom
+cat atommc2_avr.rom >> 128K.rom
+cat akernel_patched.rom >> 128K.rom
+cat bbc_c000.rom >> 128K.rom
+cat bbc_d000.rom >> 128K.rom
+cat bbc_e000.rom >> 128K.rom
+cat bbc_f000.rom >> 128K.rom
+
+cat bbc_a000.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
+cat blank.rom >> 128K.rom
