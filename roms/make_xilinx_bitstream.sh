bb895fba34481fb9731599baf8a2e66528509717
David Banks <dave@hoglet.com>
2016-07-03 20:40:23 +0100

    Updated ROMs to AtomMMC 3.00/2C, FPGAUtils 0.27 and build scripts
    Change-Id: I351f0dc6b02f2bc2ebb26414a9a53b015838478f

---
+++ roms/make_xilinx_bitstream.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+run_data2mem=true
+
+XILINX=/opt/Xilinx/14.7
+DATA2MEM=${XILINX}/ISE_DS/ISE/bin/lin/data2mem
+PAPILIO_LOADER=/opt/GadgetFactory/papilio-loader/programmer
+PROG=${PAPILIO_LOADER}/linux32/papilio-prog
+BSCAN=${PAPILIO_LOADER}/bscan_spi_xc6slx9.bit
+IMAGE=128K_avr.rom
+
+mkdir -p tmp
+
+# Build a fresh ROM image
+./make_rom_image.sh
+
+# Run bitmerge to merge in the ROM images
+gcc -o tmp/bitmerge bitmerge.c 
+./tmp/bitmerge ../xilinx/working/AtomFpga_PapilioDuo.bit 60000:$IMAGE tmp/merged.bit
+rm -f ./tmp/bitmerge
+
+# Run data2mem to merge in the AVR Firmware
+if [ $run_data2mem = "true" ]; then
+echo Merging AVR firmware
+BMM_FILE=../xilinx/AtomFpga_PapilioDuo_bd.bmm
+${DATA2MEM} -bm ${BMM_FILE} -bd avr_progmem.mem -bt tmp/merged.bit -o b tmp/merged2.bit
+mv tmp/merged2.bit tmp/merged.bit
+fi
+
+# Program the Papilo Duo
+${PROG} -v -f tmp/merged.bit -b ${BSCAN}  -sa -r
+
+# Reset the Papilio Duo
+${PROG} -c
