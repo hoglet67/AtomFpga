eff3182e91137aa633004073bf7d8170d9c3a8e0
David Banks <dave@hoglet.com>
2019-06-01 12:09:57 +0100

    Common build for A000 and E000 versions
    Change-Id: I1ea5a206fb186f067ac9d3b4eeb8c5e3261cf946

--- software/sdrom3/build.sh
+++ software/sdrom3/build.sh
@@ -2,12 +2,74 @@
 
 rm -f *.rom
 
-echo Assembling
-#ca65 -l sddos.lst -osddos.o -DAVR sdromraw.asm
-ca65 -l sddos.lst -osddos.o sdromraw.asm
+BASE=sddos3
 
-echo Linking
-ld65 sddos.o -o sddos.rom -C sddos.lkr 
+AVR_A000_ROM=${BASE}-a000-avr.rom
+AVR_E000_ROM=${BASE}-e000-avr.rom
+PIC_A000_ROM=${BASE}-a000-pic.rom
+PIC_E000_ROM=${BASE}-e000-pic.rom
 
-echo Cleaning
+
+echo Assembling AVR
+ca65 -l sddos3.a000.lst -o a000.o -DAVR sddos3.asm
+ca65 -l sddos3.e000.lst -o e000.o -DAVR -D EOOO sddos3.asm
+
+echo Linking AVR
+ld65 a000.o -o ${AVR_A000_ROM} -C sddos3-a000.lkr
+ld65 e000.o -o ${AVR_E000_ROM} -C sddos3-e000.lkr
+
+echo Removing AVR object files
 rm -f *.o
+
+echo Assembling PIC
+ca65 -l sddos3.a000.lst -o a000.o sddos3.asm
+ca65 -l sddos3.e000.lst -o e000.o -D EOOO sddos3.asm
+
+echo Linking PIC
+ld65 a000.o -o ${PIC_A000_ROM} -C sddos3-a000.lkr
+ld65 e000.o -o ${PIC_E000_ROM} -C sddos3-e000.lkr
+
+echo Removing PIC object files
+rm -f *.o
+
+for i in ${AVR_A000_ROM} ${AVR_E000_ROM} ${PIC_A000_ROM} ${PIC_E000_ROM}
+do
+    truncate -s 4096 $i
+done
+
+mkdir -p AVR
+mkdir -p PIC
+rm -f AVR/*
+rm -f PIC/*
+
+xxd -r > AVR/SDDOS3A <<EOF
+00: 41 54 4d 4d 43 33 41 00 00 00 00 00 00 00 00 00
+10: 00 A0 00 A0 00 10
+EOF
+cat ${AVR_A000_ROM} >> AVR/SDDOS3A
+mv  ${AVR_A000_ROM} AVR/SDDOS3A.rom
+md5sum AVR/SDDOS3A.rom
+
+xxd -r > AVR/SDDOS3E <<EOF
+00: 41 54 4d 4d 43 33 45 00 00 00 00 00 00 00 00 00
+10: 00 E0 00 E0 00 10
+EOF
+cat ${AVR_E000_ROM} >> AVR/SDDOS3E
+mv  ${AVR_E000_ROM} AVR/SDDOS3E.rom
+md5sum AVR/SDDOS3E.rom
+
+xxd -r > PIC/SDDOS3A <<EOF
+00: 41 54 4d 4d 43 33 41 00 00 00 00 00 00 00 00 00
+10: 00 A0 00 A0 00 10
+EOF
+cat ${PIC_A000_ROM} >> PIC/SDDOS3A
+mv  ${PIC_A000_ROM} PIC/SDDOS3A.rom
+md5sum PIC/SDDOS3A.rom
+
+xxd -r > PIC/SDDOS3E <<EOF
+00: 41 54 4d 4d 43 33 45 00 00 00 00 00 00 00 00 00
+10: 00 E0 00 E0 00 10
+EOF
+cat ${PIC_E000_ROM} >> PIC/SDDOS3E
+mv  ${PIC_E000_ROM} PIC/SDDOS3E.rom
+md5sum PIC/SDDOS3E.rom
---
+++ software/sdrom3/sddos3.asm
@@ -0,0 +1,10 @@
+.segment "CODE"
+
+.include "math-lib.inc"
+.include "atmmcdef.inc"
+.include "macros.inc"
+.include "int.inc"
+.include "sd.inc"
+
+.segment "LAST"   
+	RTS
