5a36fe67f5ac57504662cdc068d70c375121d18f
David Banks <dave@hoglet.com>
2023-05-19 12:00:46 +0100

    Gowin: Update to Dom's PSRAM controller, which works
    Change-Id: Ie2e76e8fc867fee973a009e5e8dc370f8517774a

--- gowin/AtomFpga_TangNano9K/src/psram_controller.v
+++ gowin/AtomFpga_TangNano9K/src/psram_controller.v
@@ -4,8 +4,8 @@
 //
 // This is a word or byte based, non-bursting controller for accessing the on-chip HyperRAM.
 // - 1:1 clock design. Memory and main logic work under the same clock.
-// - Low latency. Under default settings, write latency is 7 cycles (1x) or 10 cycles (2x). 
-//   Read latency is 12 cycles (1x) or 15 cycles(2x). In my test, 2x latency happens about 
+// - Low latency. Under default settings, write latency is 7 cycles (1x) or 10 cycles (2x).
+//   Read latency is 12 cycles (1x) or 15 cycles(2x). In my test, 2x latency happens about
 //   0.05% of time.
 
 module PsramController #(
@@ -20,12 +20,12 @@ module PsramController #(
     input write,              // Set to 1 to write to RAM
     input [21:0] addr,        // Byte address to read / write
     input [15:0] din,         // Data word to write
-    input byte_write,         // When writing, only write one byte instead of the whole word. 
+    input byte_write,         // When writing, only write one byte instead of the whole word.
                               // addr[0]==1 means we write the upper half of din. lower half otherwise.
     output reg [15:0] dout,   // Last read data. Read is always word-based.
     output busy,              // 1 while an operation is in progress
 
-    // HyperRAM physical interface. Gowin interface is for 2 dies. 
+    // HyperRAM physical interface. Gowin interface is for 2 dies.
     // We currently only use the first die (4MB).
     output [1:0] O_psram_ck,
     inout [1:0] IO_psram_rwds,
@@ -39,6 +39,7 @@ localparam [2:0] CONFIG_ST= 3'd1;
 localparam [2:0] IDLE_ST = 3'd2;
 localparam [2:0] READ_ST = 3'd3;
 localparam [2:0] WRITE_ST = 3'd4;
+localparam [2:0] WRITE_STOP_ST = 3'd5;
 
 reg cfg_now, dq_oen, ram_cs_n, ck_e, ck_e_p;
 reg wait_for_rd_data;
@@ -79,7 +80,7 @@ always @(posedge clk) begin
             dq_sr <= {8'h60, 8'h00, 8'h01, 8'h00, 8'h00, 8'h00, 8'h8f, CR_LATENCY, 4'h7};      // last byte, 'e' (3 cycle latency max 83Mhz), '7' (variable 1x/2x latency)
             dq_oen <= 0;
             ck_e <= 1;      // this needs to be earlier 1 cycle to allow for phase shifted clk_p
-        end 
+        end
         if (cycles_sr[4]) begin
             state <= IDLE_ST;
             ck_e <= 0;
@@ -110,7 +111,7 @@ always @(posedge clk) begin
         if (cycles_sr[3]) begin
             // command sent, now wait for result
             dq_oen <= 1;
-        end 
+        end
         if (cycles_sr[9])
             wait_for_rd_data <= 1;
         if (wait_for_rd_data && (rwds_in_ris ^ rwds_in_fal)) begin     // sample rwds falling edge to get a word / \_
@@ -122,26 +123,49 @@ always @(posedge clk) begin
     end
 
     if (state == WRITE_ST) begin
-        if (cycles_sr[5])
-            additional_latency <= rwds_in_fal;  // sample RWDS to see if we need additional latency
+//        if (cycles_sr[5])
+//            additional_latency <= rwds_in_fal;  // sample RWDS to see if we need additional latency
+//        // Write timing is trickier - we sample RWDS at cycle 5 to determine whether we need to wait another tACC.
+//        // If it is low, data starts at 2+LATENCY. If high, then data starts at 2+LATENCY*2.
+//        if (cycles_sr[2+LATENCY] && (LATENCY == 3 ? ~rwds_in_fal : ~additional_latency)
+//            || cycles_sr[2+LATENCY*2])
+//        if (cycles_sr[5])
+//            additional_latency <= rwds_in_fal;  // sample RWDS to see if we need additional latency
         // Write timing is trickier - we sample RWDS at cycle 5 to determine whether we need to wait another tACC.
         // If it is low, data starts at 2+LATENCY. If high, then data starts at 2+LATENCY*2.
-        if (cycles_sr[2+LATENCY] && (LATENCY == 3 ? ~rwds_in_fal : ~additional_latency)
-            || cycles_sr[2+LATENCY*2])
+
+        if (cycles_sr[5])
+            additional_latency <= IO_psram_rwds[0];  // sample RWDS to see if we need additional latency
+        else if (cycles_sr[2+LATENCY-1])
         begin
+            rwds_oen <= 0;
+            rwds_out_ris <= 1'b0;       // RWDS preamble
+            rwds_out_fal <= 1'b0;
+        end
+        else if ((cycles_sr[2+LATENCY] && ~additional_latency)
+            || cycles_sr[2+LATENCY*2]) begin
+//        else if(cycles_sr[2+LATENCY]) begin
             rwds_oen <= 0;
             rwds_out_ris <= byte_write ? ~addr[0] : 1'b0;       // RWDS is data mask (1 means not writing)
             rwds_out_fal <= byte_write ? addr[0] : 1'b0;
             dq_sr[63:48] <= w_din;
-            state <= IDLE_ST;
+            state <= WRITE_STOP_ST;
         end
+    end
+
+    if (state == WRITE_STOP_ST)
+    begin
+        rwds_oen <= 1;
+        ram_cs_n <= 1;
+        ck_e <= 0;
+        state <= IDLE_ST;
     end
 
     if (~resetn) begin
         state <= INIT_ST;
         ram_cs_n <= 1;
         ck_e <= 0;
-    end 
+    end
 end
 
 
@@ -152,13 +176,13 @@ end
 localparam INIT_TIME = FREQ / 1000 * 160 / 1000;
 reg  [$clog2(INIT_TIME+1):0]   rst_cnt;
 reg rst_done, rst_done_p1, cfg_busy;
-  
+
 always @(posedge clk) begin
     rst_done_p1 <= rst_done;
     cfg_now     <= rst_done & ~rst_done_p1;// Rising Edge Detect
 
     if (rst_cnt != INIT_TIME) begin      // count to 160 us
-        rst_cnt  <= rst_cnt[14:0] + 1'b1;
+        rst_cnt  <= rst_cnt[14:0] + 1;
         rst_done <= 0;
         cfg_busy <= 1;
     end else begin
@@ -215,5 +239,3 @@ generate
 endgenerate
 
 endmodule
-
-
