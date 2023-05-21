b413bd34211a113c191510861a1d6df16d0a310b
David Banks <dave@hoglet.com>
2023-05-21 12:34:18 +0100

    Gowin: Check in smaller 16K ROM image
    Change-Id: I17213e1b8df4c27fc3ac6c8b7709d8b300db0411

--- roms/make_ramrom_tang_image.sh
+++ roms/make_ramrom_tang_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function build_rom {
+function build_64k_rom {
 
 ROM=$1
 AVR=$2
@@ -33,4 +33,24 @@ cat akernel_patched.rom >> ${ROM}
 
 }
 
-build_rom "64K_avr.rom" "_avr"
+function build_16k_rom {
+
+ROM=$1
+AVR=$2
+
+echo Building ${ROM}
+
+rm -f ${ROM}
+touch ${ROM}
+
+cat abasic.rom >> ${ROM}
+cat afloat_patched.rom >> ${ROM}
+cat atommc3${AVR}.rom >> ${ROM}
+#cat ../software/sdrom/SDROM.rom >> ${ROM}
+cat akernel_patched.rom >> ${ROM}
+
+}
+
+build_64k_rom "64K_avr.rom" "_avr"
+
+build_16k_rom "16K_avr.rom" "_avr"
