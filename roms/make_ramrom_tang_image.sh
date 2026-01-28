9921bd490d87b1c4f58c960880a2efbe705defab
David Banks <dave@hoglet.com>
2026-01-28 08:04:51 +0000

    ROMs: Build 128K tang image that includes FPGA version of SDDOS ROM
    Change-Id: I5f8827194a78dda31e566723c7ee39c9d1adf5b9

--- roms/make_ramrom_tang_image.sh
+++ roms/make_ramrom_tang_image.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+function build_128k_rom {
+
+ROM=$1
+AVR=$2
+
+echo Building ${ROM}
+
+rm -f ${ROM}
+touch ${ROM}
+
+cat sddos-v3.27${AVR}.rom >> ${ROM}
+cat gags_v2.3.rom >> ${ROM}
+cat pcharme_v1.73.rom >> ${ROM}
+cat axr1.rom >> ${ROM}
+cat fpgautils.rom >> ${ROM}
+cat atomic_windows_v1.1.rom >> ${ROM}
+cat we_rom.rom >> ${ROM}
+cat pp_toolkit.rom >> ${ROM}
+
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+
+cat abasic.rom >> ${ROM}
+cat afloat_patched.rom >> ${ROM}
+cat atommc3${AVR}.rom >> ${ROM}
+cat akernel_patched.rom >> ${ROM}
+
+cat abasic.rom >> ${ROM}
+cat afloat_patched.rom >> ${ROM}
+cat ../software/sdrom/SDROM_FPGA.rom >> ${ROM}
+cat akernel_patched.rom >> ${ROM}
+
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+cat blank.rom >> ${ROM}
+
+}
+
 function build_64k_rom {
 
 ROM=$1
@@ -22,13 +71,11 @@ cat pp_toolkit.rom >> ${ROM}
 cat abasic.rom >> ${ROM}
 cat afloat_patched.rom >> ${ROM}
 cat atommc3${AVR}.rom >> ${ROM}
-#cat ../software/sdrom/SDROM.rom >> ${ROM}
 cat akernel_patched.rom >> ${ROM}
 
 cat abasic.rom >> ${ROM}
 cat afloat_patched.rom >> ${ROM}
 cat atommc3${AVR}.rom >> ${ROM}
-#cat ../software/sdrom/SDROM.rom >> ${ROM}
 cat akernel_patched.rom >> ${ROM}
 
 }
@@ -46,17 +93,18 @@ touch ${ROM}
 cat abasic.rom >> ${ROM}
 cat afloat_patched.rom >> ${ROM}
 cat atommc3${AVR}.rom >> ${ROM}
-#cat ../software/sdrom/SDROM.rom >> ${ROM}
 cat akernel_patched.rom >> ${ROM}
 
 }
+
+build_128k_rom "128K_avr.bin" "_avr"
 
 build_64k_rom "64K_avr.bin" "_avr"
 
 build_16k_rom "16K_avr.bin" "_avr"
 
 # Convert to ascii binary
-for file in 16K_avr 64K_avr
+for file in 16K_avr 64K_avr 128K_avr
 do
     xxd  -c1 -b $file.bin | awk '{print $2}' > $file.bit
 done
